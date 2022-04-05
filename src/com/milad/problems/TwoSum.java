5022ef5534082c33ebbe2a8b5f6fe6c39a329ae3
Milad <millad.99.awad@gmail.com>
2022-04-05 16:45:13 +0300

    Add TwoSum solution

---
+++ src/com/milad/problems/TwoSum.java
@@ -0,0 +1,27 @@
+package com.milad.problems;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> numsMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (numsMap.containsKey(nums[i])) {
+                int currentMapVal = numsMap.get(nums[i]);
+
+
+                return new int[]{currentMapVal, i};
+            } else {
+
+
+                int numberToFind = target - nums[i];
+                numsMap.put(numberToFind, i);
+            }
+        }
+        return null;
+
+
+    }
+}
